60ed99ca7ee01fa7b024121ffe47d95dfca221de
Tolga Mizrakci <tolgamizrakci@Tolgas-MacBook-Pro.local>
2019-03-30 19:48:01 -0400

    server working

--- server/controllers/eventControllers.js
+++ server/controllers/eventControllers.js
@@ -1,14 +1,27 @@
-const { Client } = require('pg');
+const db = require('./db.js')
 
-const client = new Client({
-  user: 'tolgagrouse',
-  host: 'localhost',
-  database: 'jest'
-});
+let queryString = 'SELECT * FROM events';
+let insertString = 'INSERT INTO events'
 
-client.connect(err => {
-  if (err) console.log(err);
-  else console.log('Client connected to the database.');
-});
+const find = (req, res) => {
+  db.query(queryString, (err, db) => {
+    if (err) {
+      console.log('theres an error')
+    } else {
+      res.send(db.rows);
+    }
+  })
+}
 
-module.exports = client;
+// const insert = (req, res) => {
+//   db.query(queryString, (err, db) => {
+//     if (err) {
+//       console.log('theres an error')
+//     } else {
+//       res.send(db.rows);
+//     }
+//   })
+// }
+
+
+module.exports = { find };
--- server/server.js
+++ server/server.js
@@ -4,5 +4,4 @@ const api = require('./routes/api');
 
 app.use('/api', api);
 
-
 app.listen(3000, () => console.log('Listening on port 3000'));
