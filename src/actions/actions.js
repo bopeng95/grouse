b37baf680d728ef3b9e3d238d1e87efa52b6647d
Nel Malikova <gunelmalikova@gmail.com>
2019-04-01 10:28:52 -0400

    pre-merge

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,32 +2,33 @@ import types from './actionTypes';
 import keywords from './keywords';
 
 export default {
-    addTextAndTags: (text) => ({
-        type: types.ADD_TEXT_AND_TAGS,
-        payload: getTagsFromText(text)
-    }),
-    getTextAndTags: () => {
-        fetch('/api')
-        .then(res => res.json())
-        .then(data => {
-            return {
-                type: types.GET_TEXT_AND_TAGS,
-                payload: data
-            }
-        });
-    },
-}
+  addTextAndTags: text => ({
+    type: types.ADD_TEXT_AND_TAGS,
+    payload: getTagsFromText(text)
+  }),
+  getTextAndTags: () => {
+    fetch('/api/messages')
+      .then(res => res.json())
+      .then(data => {
+        return {
+          type: types.GET_TEXT_AND_TAGS,
+          payload: data
+        };
+      });
+  }
+};
 
 function getTagsFromText(text) {
-    const data = { text: text, date: Date.now() };
-    //retrieve tags from text logic here
-    data.tags = retrieveTags(text.toLowerCase());
-    return data;
+  const data = { text: text, date: Date.now() };
+  //retrieve tags from text logic here
+  data.tags = retrieveTags(text.toLowerCase());
+  return data;
 }
 
 function retrieveTags(txt) {
-    const tags = [];
-    for(const key in keywords) {
-        if(txt.indexOf(key) !== -1) tags.push(key);
-    } return tags;
+  const tags = [];
+  for (const key in keywords) {
+    if (txt.indexOf(key) !== -1) tags.push(key);
+  }
+  return tags;
 }
