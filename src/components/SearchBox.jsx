a6aaf502fc85e66d892b0649e633dd8e78324e36
Nel Malikova <gunelmalikova@gmail.com>
2019-04-01 14:24:32 -0400

    wired up the search button

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,22 +3,38 @@ import React, { Component } from 'react';
 class SearchBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchQuery: ''
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
   }
 
   handleSubmit(event) {
     // search query logic here
     event.preventDefault();
     // query the database for keyword, return messages containing the keywords that the user searched for
+
+    this.setState({ searchQuery: '' });
   }
+
   render() {
     return (
       <form onSubmit={event => this.handleSubmit(event)}>
-        <input type='text' placeholder='Search for...' name='searchQuery' />
         <input
-          className='btn btn-outline-success'
+          onChange={this.handleChange}
+          type='text'
+          placeholder='Search for...'
+          name='searchQuery'
+          value={this.state.searchQuery}
+        />
+        <input
+          className='btn btn-outline-primary'
           type='submit'
           value='Submit'
         />
